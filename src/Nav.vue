11237388e479cb752f1b2fe1db2eafe09025cde0
pko <peterrko1004@gmail.com>
2020-03-09 01:05:25 -0400

    added vue nav bar

---
+++ src/Nav.vue
@@ -0,0 +1,66 @@
+<template>
+  <nav class="flex flex-auto w-100 z-3">
+      <div class="step-nav">
+        <a href="#" class="is-active"><span>Chair</span></a>
+        <a href="#"><span>Table</span></a>
+        <a href="#"><span>Sizes</span></a>
+        <a href="#"><span>Summary</span></a>
+      </div>
+    </nav>
+</template>
+
+
+
+<script>
+
+
+
+</script>
+
+<style>
+
+.step-nav{
+  height: 100%
+}
+
+.step-nav a:hover{
+  border-bottom: 3px outset black;
+  text-decoration-style: none;
+}
+
+.builder{
+  display: inline-block;
+}
+
+.builder nav{
+    position: fixed;
+    background: #fff;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    border-bottom: 1px solid #ddd;
+    z-index: 1;
+    height: 35px;
+    color: black;
+    margin: auto;
+}
+.builder .step-nav a {
+    font-size: 13px;
+    padding: 0 1.25rem;
+    color: #777;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    overflow: hidden;
+    text-decoration:none;
+}
+.builder nav div:last-child {
+    justify-content: center;
+    flex: 1 auto;
+}
+.builder nav div {
+    display: flex;
+}
+.builder .step-nav a.is-active {
+    color: #000;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-import { Slide } from 'vue-burger-menu'  // import the CSS transitions you wish to use, in this case we are using `Slide`
-
-export default {
-    components: {
-        Slide // Register your component
-    }
-}
+import Nav from './Nav.vue'
 
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+new Vue({
+  el: '#nav-bar',
+  render: h => h(Nav)
 })
