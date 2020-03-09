9419e08e5cb14a8ecf742602492229e37c15fd87
pko <peterrko1004@gmail.com>
2020-03-09 13:22:23 -0400

    working click based canvas

--- src/main.js
+++ src/main.js
@@ -25,6 +25,49 @@ new Vue({
   el: '#canvas',
   render: h => h(Canvas)
 })
+new Vue({
+	el: '#selection',
+
+	data: {
+	  src: ""
+	},
+
+	methods: {
+	  copySrc: function(img){
+	      this.src = img.srcElement.src;
+	      var width = window.innerWidth;
+	      var height = window.innerHeight;
+
+	      var stage = new Konva.Stage({
+	        container: 'canvas-container',
+	        width: width,
+	        height: height
+	      });
+
+	      var layer = new Konva.Layer();
+	      stage.add(layer);
+
+	      // main API:
+	      var imageObj = new Image();
+	      imageObj.onload = function() {
+	        var yoda = new Konva.Image({
+	          x: 50,
+	          y: 50,
+	          image: imageObj,
+	          scaleX: 0.5,
+	          scaleY: 0.5
+	        });
+
+	        // add the shape to the layer
+	        layer.add(yoda);
+	        layer.batchDraw();
+	      };
+
+
+	      imageObj.src = this.src;
+	  }
+	}
+});
 
 // new Vue({
 //   el: '#imageconvert',
