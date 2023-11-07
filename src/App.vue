ce638eca82f21034d28711b9d2bead2c7619a8bb
Peter Ko <pko@wsgc.com>
2023-11-06 22:37:45 -0500

    push

--- src/App.vue
+++ src/App.vue
@@ -1,164 +1,28 @@
 <template>
-    <Slide>
-      <a id="home" href="#">
-        <span class="glyphicon glyphicon-home"></span>
-      </a>
-      <a id="store" href="#">
-        <span class="glyphicon glyphicon-shopping-cart"></span>
-      </a>
-      <a id="user" href="#">
-        <span class="glyphicon glyphicon glyphicon-user"></span>
-      </a>      
-      <a id="contact" href="#">
-        <span class="glyphicon glyphicon-phone-alt"></span>
-      </a>      
-    </Slide>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
-
-
 <script>
-import { Slide } from 'vue-burger-menu'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-    components: {
-        Slide 
-    }
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
 <style>
-.bm-overlay {
-   background:white;
-}
-.bm-burger-button {
-    cursor: pointer;
-    height: 20px;
-    left: 36px;
-    position: absolute;
-    top: 25px;
-    width: 25px;
-    z-index: 1;
-    position:fixed;
-}
-
-.bm-item-list>* {
-    display: inline-flex;
-    padding: .7em;
-    text-decoration: none;
-}
-
-.bm-item-list>*>span {
-    color: #9a7c88;
-    font-weight: 700;
-    margin-left: 10px;
-    font-size: 20px;
-    top:5px;
-    width: 0px;
-    text-align: center;
-}
-
-.bm-cross {
-  background: #d2c0c4;
-  transform: rotate(90deg) !important;
-}
-
-.bm-menu{
-  background-color:#e6dadaba;
-  height:60px;
-  overflow:hidden;
-  position:fixed;
-}
-.bm-item-list a:hover{
-  color:none;
-}
-
-.bm-item-list a:active{
-  color:#b7afb2;
-}
-.cross-style{
-  top: 28px;
-  left: 48px;
-}
-.bm-item-list {
-    color: #b8b7ad;
-    font-size: 20px;
-    margin-left: 20%;
-    top: -55px;
-    position: relative;
-}
-.bm-burger-bars {
-    background-color: #9a7c88;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-body{
-  overflow-anchor: none;
-    font-family: "GT-Eesti 3r",Arial,sans-serif;
-    font-size: 14px;
-    background-color: #fff;
-    color: #363636;
-}
-#heading{
-  font-size:30px;
-  margin: auto;
-  position: fixed;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background: #e6dada;
-  height: 70px;
-  padding-top: 5px;
-  z-index: 1;
-  width: 100%;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-.bm-burger-button{
-  top:20px;
-}
-.main-area{
-  position: relative;
-  top: 30px;
-}
-.canvas{
-  text-align: center;
-
-}
-.select-item-section{
-  padding:25px;
-  background:white;
-  padding-top: 50px;
-  padding-left:40px !important;
-  padding-right:40px !important;
-}
-
-#all-the-drawings{
-  background:#e5e5e5;
-}
-
-.konvajs-content{
-  position:fixed !important;
-  top:15%;
-}
-
-.row{
-  max-width:1280px;
-  justify-content: center;
-  text-align:center;
-  margin: auto;
-}
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,72 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Nav from './Nav.vue'
-import Selection from './Selection.vue'
-import ChairSelection from './ChairSelection.vue'
-import LightSelection from './LightSelection.vue'
-// import Furniture from './Furniture.vue'
-// import Canvas from './Canvas.vue'
-// import VueKonva from 'vue-konva';
-// Vue.use(VueKonva);
+
+Vue.config.productionTip = false
 
 new Vue({
- 	 el: '#app',
- 	 render: h => h(App)
-})
-
-new Vue({
-  	el: '#nav-bar',
-  	render: h => h(Nav)
-})
-
-new Vue({
-	el: '#selection',
-	render: h => h(Selection)
-});
-new Vue({
-	el: '#chairselection',
-	render: h => h(ChairSelection)
-});
-
-new Vue({
-	el: '#lightselection',
-	render: h => h(LightSelection)
-});
-
-
-
-// new Vue({
-//   el: '#imageconvert',
-//   data: {
-//     url: '',
-//     base64: '',
-//     error: ''
-//   },
-//   methods: {
-//     generateBase64: function generateBase64() {
-//       var _this = this;
-
-//       var canvas = document.createElement('CANVAS');
-//       var img = document.createElement('img');
-//       img.src = this.url;
-//       img.onload = function () {
-//         canvas.height = img.height;
-//         canvas.width = img.width;
-//         _this.base64 = canvas.toDataURL('image/png');
-//         canvas = null;
-//       };
-
-//       img.onerror = function (error) {
-//         _this.error = 'Could not load image, please check that the file is accessible and an image!';
-//       };
-//     }
-//   },
-//   watch: {
-//     url: function url() {
-//       this.base64 = '';
-//       this.error = '';
-//     }
-//   }
-// });
-
-// Vue.config.devtools = false;
+  render: h => h(App),
+}).$mount('#app')
